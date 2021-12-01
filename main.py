274b3f5706a7a53253ab6c7d5cc8bb025ca1a1a6
MateuszBar <matibaran3141@gmail.com>
2021-12-01 11:24:05 +0000

    Add __init__ func with TODO

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 class webdriver():
+    def __init__(self):
+        #TODO: create constructor which initialises webdriver and web adress         
+        pass
+
     def search_term():
         #TODO: create function that searches for a term in the websites search bar and clicks "see all job results button"
         pass
@@ -6,11 +10,8 @@ class webdriver():
     def collect_job_list():
         #TODO: create function that looks for element that contains the list of job postings on the current page
         pass
-    
+
     def find_next_page():
         #TODO: create function that finds next page element 
         pass
     
-    
-
-
