21c614fefd9da10dd45ed04135935fe9e3648466
MateuszBar <matibaran3141@gmail.com>
2021-12-01 11:12:54 +0000

    Add scroll_down and search_term funcs with TODOs

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 class webdriver():
-    
+    def scroll_down():
+        #TODO: create function that scrolls down the search results of the webpage
+        pass
+
+    def search_term():
+        #TODO: create function that searches for a term in the websites search bar
+        pass
