3a97026441feabfa2425ecf4b6449263f1ef10fd
MateuszBar <matibaran3141@gmail.com>
2021-12-01 11:14:46 +0000

    Remove scroll_down func as it is not needed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 class webdriver():
-    def scroll_down():
-        #TODO: create function that scrolls down the search results of the webpage
-        pass
-
+    
     def search_term():
         #TODO: create function that searches for a term in the websites search bar
         pass
+
