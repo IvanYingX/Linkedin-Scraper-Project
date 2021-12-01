49ea1e0fe594d447afee7286e0b53b01f39299ac
MateuszBar <matibaran3141@gmail.com>
2021-12-01 11:21:53 +0000

    Add collect_job_list func with TODO

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 class webdriver():
     def search_term():
-        #TODO: create function that searches for a term in the websites search bar
+        #TODO: create function that searches for a term in the websites search bar and clicks "see all job results button"
+        pass
+    
+    def collect_job_list():
+        #TODO: create function that looks for element that contains the list of job postings on the current page
         pass
     
     def find_next_page():
         #TODO: create function that finds next page element 
         pass
     
+    
+
+
