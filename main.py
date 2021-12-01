5df2e9e89526b270414467b54a153e76dd70e464
MateuszBar <matibaran3141@gmail.com>
2021-12-01 11:17:33 +0000

    Add find_next_page func with TODO

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 class webdriver():
-    
     def search_term():
         #TODO: create function that searches for a term in the websites search bar
         pass
-
+    
+    def find_next_page():
+        #TODO: create function that finds next page element 
+        pass
+    
