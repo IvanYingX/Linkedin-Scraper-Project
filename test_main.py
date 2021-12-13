1c242b99116459cb5b07aef2311868916ff8fc91
MateuszBar <matibaran3141@gmail.com>
2021-12-13 12:15:51 +0000

    Add test for accept_cookies and log_me_in

--- test_main.py
+++ test_main.py
@@ -1,26 +1,42 @@
 from main import WebDriver
 import unittest
 from selenium.webdriver.chrome.options import Options
+from time import sleep
 
 
 class WebDriverTestCase(unittest.TestCase):
-    # Initialise scenario for test
+    '''Test Class for testing WebDriver class from main script'''
     def setUp(self):
-        username = "AiCoreOct2021@outlook.com"
-        password = "Password123,,"
-        self.website = "https://www.linkedin.com/feed/"
+        '''Setting up an instance of the webdriver class for testing'''
+        self.username = "AiCoreOct2021@outlook.com"
+        self.password = "Password123,,"
+        self.website = "https://www.linkedin.com/signup/cold-join?session_redirect=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2Ffeed%2F&trk=login_reg_redirect"
         chrome_options = Options()
         chrome_options.add_experimental_option("detach", True)
-        self.instance = WebDriver(chrome_options, self.website, username, password)
+        self.instance = WebDriver(chrome_options, self.website, self.username, self.password)
+        self.instance.driver.get(self.website)
 
     def test_get_current_url(self):
         expected_result = "https://www.linkedin.com/signup/cold-join?session_redirect=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2Ffeed%2F&trk=login_reg_redirect"
-        self.instance.driver.get(self.website)
-        actual_value = self.instance.get_current_url()
-        self.assertEqual(expected_result, actual_value)
+        actual_result = self.instance.get_current_url()
+        self.assertEqual(expected_result, actual_result)
+
+    def test_accept_cookies(self):
+        sleep(5)
+        self.instance.accept_cookies()
+
+    def test_log_me_in(self):
+        sleep(5)
+        self.instance.accept_cookies()
+        sleep(5)
+        self.instance.log_me_in()
+        expected_result = "https://www.linkedin.com/feed/"
+        actual_result = self.instance.driver.current_url
+        self.assertEqual(expected_result, actual_result)
 
     def tearDown(self):
-        del self.instance
+        sleep(5)
+        self.instance.driver.quit()
 
 
 unittest.main(argv=[''], verbosity=2, exit=False)
