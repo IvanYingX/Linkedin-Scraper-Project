ccb586caf443a15ee0c745fb776ec2e786be4421
MateuszBar <matibaran3141@gmail.com>
2021-12-13 11:43:46 +0000

    Add SetUp and TearDown functions

--- test_main.py
+++ test_main.py
@@ -1,5 +1,18 @@
 from main import WebDriver
 import unittest
+from selenium.webdriver.chrome.options import Options
 
 class WebDriverTestCase(unittest.TestCase):
-    pass
+    def setUp(self):
+        username = "AiCoreOct2021@outlook.com"
+        password = "Password123,,"
+        website = "https://www.linkedin.com/feed/"
+        chrome_options = Options()
+        chrome_options.add_experimental_option("detach", True)
+        self.instance = WebDriver(address=website,username=username,password=password)
+
+    def test_get_current_url(self):
+        pass
+
+    def tearDown(self):
+        del self.instance
