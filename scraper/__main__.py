5984fc44ac0446f2bce2396c91d6e13797b6969e
MateuszBar <matibaran3141@gmail.com>
2022-01-20 19:15:49 +0000

    Clean up __main__.py

--- scraper/__main__.py
+++ scraper/__main__.py
@@ -1,6 +1,5 @@
 #import all methods from method.py file
 from methods import *
- 
 
 def main():
     '''
@@ -13,7 +12,7 @@ def main():
     chrome_options.add_experimental_option("detach", True)
     chrome_options.add_argument("--start-maximized")
     #chrome_options.add_argument("--headless")
-    scraper = WebDriver(chrome_options, website, username, password) 
+    scraper = WebDriver(chrome_options, website, username, password)
     scraper.driver.implicitly_wait(2)
     scraper.driver.get(website)
     sleep(3)
@@ -21,19 +20,14 @@ def main():
     sleep(2)
     scraper.log_me_in()
     sleep(2)
+    # Edit this to change search term and location
     scraper.search_term('Data Science', 'London')
     sleep(2)
     scraper.extract_job_details()
     sleep(2)
-    #scraper.dataframe_to_csv(my_dataframe)
     print("\n\nScraping has been completed")
+
 if __name__ == "__main__":
     # safeguard used to prevent script running
     # automatically if it's imported into another file
     main()
-    
-
-# username = "aicorebot2@outlook.com"
-# password = "aicoreteam2"
-# username = "AiCoreOct2021@outlook.com"
-# password = "Password123,,"
