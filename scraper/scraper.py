3e59757f2b18b238568dc08379611509b2af3075
Joseph Solomon <elhakimjosephsolomon@gmail.com>
2022-01-21 09:55:42 +0000

    Include captcha workaround

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -9,9 +9,10 @@ def main():
 
     website = "https://www.linkedin.com/feed/"
     chrome_options = Options()
-    #chrome_options.add_experimental_option("detach", True)
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
     chrome_options.add_argument('--window-size=1920,1080')
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument(f'user-agent={user_agent}')
     scraper = WebDriver(chrome_options, website, LINKEDINUSERNAME, LINKEDINPASSWORD)
     scraper.driver.implicitly_wait(2)
     scraper.driver.get(website)
@@ -26,6 +27,7 @@ def main():
     scraper.extract_job_details()
     sleep(2)
     print("\n\nScraping has been completed")
+    scraper.driver.quit()
 
 if __name__ == "__main__":
     # safeguard used to prevent script running
